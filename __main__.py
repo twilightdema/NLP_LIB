c4aaaf129f081e20a4a3896178b3edce17b483da
nok <nok@pop-os.localdomain>
2020-02-23 00:27:24 +0700

    Add main

---
+++ __main__.py
@@ -0,0 +1,6 @@
+import sys
+from NLP_LIB.core import Framework
+
+# Main entry point
+if __name__ == '__main__':
+  Framework().run(sys.argv)
--- core.py
+++ core.py
@@ -1,10 +1,15 @@
+import sys
+
 class Framework:
 
   def __init__(self):
     pass
 
-  def execute(self, command_string):
+  def run(self, command_string):
     from NLP_LIB.nlp_core.engine import main
     command_arr = command_string.split(' ')
     main(command_arr)
 
+# Main entry point
+if __name__ == '__main__':
+  Framework().run(sys.argv)

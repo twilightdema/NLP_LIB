5360902eba78baf397fe04aebb3259d490e47805
twilightdema@gmail.com <twilightdema@gmail.com>
2020-11-05 09:24:52 +0700

    Add code for Matched Averaging

--- nlp_core/engine.py
+++ nlp_core/engine.py
@@ -242,7 +242,6 @@ class NLPEngine:
         federated_weights = federated_model.get_weights()
         federated_weights_list.append(federated_weights)
 
-      # [TODO]: Perform federated averaging on model weights
       print('[INFO] Finished federated training of epoch: ' + str(epoch))
       new_weights = list()
 
@@ -286,6 +285,159 @@ class NLPEngine:
         print('[INFO] Saving averaged weight at node ' + str(node_id) + ': ' + last_checkpoint_filepath)
         federated_model.save_weights(last_checkpoint_filepath)
 
+  def run_train_matching_federated_simulation(self, config, node_count):
+    print('[INFO] Start running weight matching federated training simulation on ' + str(node_count) + ' node(s).')
+
+    # Load some parameters of execution_config as they may have to be instruments
+    execution = config['execution']
+    execution_config = execution['config']    
+    base_output_dir = os.path.join(execution_config['output_dir'], 'mftrain_' + str(node_count))
+    if not os.path.exists(base_output_dir):
+      os.makedirs(base_output_dir)
+    base_epoch = execution_config['epochs']
+
+    # The process of federated simulation is that we will train model on each node epoch-by-epoch.
+    # After each epoch we will load trained model of each node and perform federated averaging on their weights.
+    # We then save averaged weights to latest checkpoint of model in each node and proceed to next epoch.
+
+    # Tensorboard log directory
+    dir_suffix = '' # We do not use gpu_count in save path anymore
+    tboard_log_dir = os.path.join(base_output_dir, 'tboard_log' + dir_suffix)
+    if not os.path.exists(tboard_log_dir):
+      os.makedirs(tboard_log_dir)
+    log_writer = tf.summary.FileWriter(tboard_log_dir)
+
+    for epoch in range(base_epoch):
+      print('[INFO] Federated training epoch: ' + str(epoch))
+
+      # Avoid memory leakage in Tensorflow / Keras
+      K.clear_session()    
+
+      federated_weights_list = []
+      federated_weights_names = []
+      federated_model = None
+      x_valid_feed = None
+      y_valid_feed = None
+      metric_names = None
+
+      for node_id in range(node_count):
+        print('[INFO] Running epoch ' + str(epoch) + ' of node: ' + str(node_id))
+        dataset = config['dataset']
+        dataset_class = dataset['class']
+        dataset_config = dataset['config']
+        dataset_class = getattr(self.datasets_module, dataset_class)
+        dataset = dataset_class(dataset_config)
+
+        federated_dataset = FederatedData(config, dataset, node_count, node_id) 
+
+        input_transform = config['input_transform']
+        input_transform_class = input_transform['class']
+        input_transform_config = input_transform['config']
+        input_transform_class = getattr(self.transforms_module, input_transform_class)
+        input_transform = input_transform_class(input_transform_config, federated_dataset)
+
+        output_transform = config['output_transform']
+        output_transform_class = output_transform['class']
+        output_transform_config = output_transform['config']
+        output_transform_class = getattr(self.transforms_module, output_transform_class)
+        output_transform = output_transform_class(output_transform_config, federated_dataset)
+
+        model = config['model']
+        model_class = model['class']
+        model_config = model['config']
+        model_class = getattr(self.models_module, model_class)
+        model = model_class(model_config, input_transform, output_transform)
+
+        # Change epoch to let each node train incrementally epoch-by-epoch
+        execution_config['epochs'] = (epoch + 1) 
+
+        # Some optimizer has LR calculation that require final number of epoch to calculate effetive LR correctly.
+        # Example is BERT warmup learning rate. We store such information here.
+        execution_config['final_epochs'] = base_epoch
+
+        # Change output directory to be include node_id so we save model from each node separately
+        execution_config['output_dir'] = os.path.join(*re.split('/|\\\\', base_output_dir), 'federated_' + str(node_id))
+
+        callbacks_ = config['callbacks']
+        callbacks = []
+        for callback in callbacks_:
+          callback_class = callback['class']
+          callback_config = callback['config']
+          callback_class = getattr(self.callbacks_module, callback_class)
+          callback = callback_class(callback_config, execution_config, model, federated_dataset, input_transform, output_transform)
+          callbacks.append(callback)
+
+        training = TrainingWrapper(model, input_transform, output_transform, callbacks, execution_config)        
+        federated_model, x_valid, y_valid = training.train(federated_dataset)
+
+        # Store validation data for used in federated evaluation
+        if x_valid_feed is None or y_valid_feed is None:
+          x_valid_feed = x_valid
+          y_valid_feed = y_valid
+        else:
+          for i, (x, xn) in enumerate(zip(x_valid_feed, x_valid)):
+            x_valid_feed[i] = np.append(x, xn, 0)
+          for i, (y, yn) in enumerate(zip(y_valid_feed, y_valid)):
+            y_valid_feed[i] = np.append(y, yn, 0)
+
+        metric_names = training.trainable_model.get_metric_names()
+
+        federated_weights = federated_model.get_weights()
+        federated_weights_list.append(federated_weights)
+
+        # Get name information from 1st local model only (as they are the same for every local model)
+        if node_id == 0:
+          federated_weights_names = [weight.name for layer in federated_model.layers for weight in layer.weights]
+
+
+      print(federated_weights_names)
+      print(len(federated_weights_list[0]))
+      print(len(federated_weights_names))
+      exit(0)
+
+      # [TODO]: Perform matched federated averaging on model weights
+      print('[INFO] Finished weight matching federated training of epoch: ' + str(epoch))
+      new_weights = list()
+
+      print('[INFO] Perform federated weight aggregation for epoch: ' + str(epoch))
+      for weights_list_tuple in zip(*federated_weights_list):
+          new_weights.append(np.array([np.array(weights_).mean(axis=0) for weights_ in zip(*weights_list_tuple)]))
+      new_weights = np.array(new_weights)
+      federated_model.set_weights(new_weights)
+      # Save the averaged weight to center checkpoint
+      checkpoint_dir = os.path.join(base_output_dir, 'checkpoint')
+      if not os.path.exists(checkpoint_dir):
+        os.makedirs(checkpoint_dir)
+      dir_suffix = '' # We do not use gpu_count in save path anymore
+      last_checkpoint_filepath = os.path.join(checkpoint_dir, 'last_weight' + dir_suffix + '.h5')
+      print('[INFO] Saving averaged weight at: ' + last_checkpoint_filepath)
+      federated_model.save_weights(last_checkpoint_filepath)
+
+      # Perform averaged model evaluation here!
+      print('Evaluate with federated evaluation dataset of size: ' + str(x_valid_feed[0].shape))
+      metrics = federated_model.evaluate(
+        x=x_valid_feed, y=y_valid_feed,
+        batch_size=execution_config['batch_size']
+      )
+
+      summary_vals = [tf.Summary.Value(tag="loss", simple_value=metrics[0])]
+      for i in range(len(metric_names)):
+        summary_vals.append(tf.Summary.Value(tag=metric_names[i], simple_value=metrics[i + 1]))
+      summary = tf.Summary(value=summary_vals)      
+      log_writer.add_summary(summary, epoch)
+      log_writer.flush()
+
+      print('==== FEDETATED EVALUATION RESULTS ====')
+      print(metrics)
+
+      # Also save the averaged model to every federated node. This is equal to serialize and send updated model to every node.
+      for node_id in range(node_count):
+        output_dir = os.path.join(*re.split('/|\\\\', base_output_dir), 'federated_' + str(node_id))
+        checkpoint_dir = os.path.join(output_dir, 'checkpoint')
+        dir_suffix = '' # We do not use gpu_count in save path anymore
+        last_checkpoint_filepath = os.path.join(checkpoint_dir, 'last_weight' + dir_suffix + '.h5')
+        print('[INFO] Saving averaged weight at node ' + str(node_id) + ': ' + last_checkpoint_filepath)
+        federated_model.save_weights(last_checkpoint_filepath)
 
   def run_prediction(self, mode, sampling_algorithm, generation_count, config, input_mode, input_path):
     print('Running in ' + mode + ' mode for input_mode = ' + input_mode + ', input_path = ' + input_path)
@@ -449,6 +601,10 @@ def main(argv):
     node_count = int(mode[len('ftrain:'):])
     print('[INFO] Perform Federated Training Simulation on ' + str(node_count) + ' node(s).')
     engine.run_train_federated_simulation(execution_config, node_count)
+  elif mode.startswith('mftrain:'):
+    node_count = int(mode[len('mftrain:'):])
+    print('[INFO] Perform Weight Matching Federated Training Simulation on ' + str(node_count) + ' node(s).')
+    engine.run_train_matching_federated_simulation(execution_config, node_count)
   elif mode == 'predict' or mode == 'generate':
     (Y_output, Y_id_max, Y) = engine.run_prediction(mode, sampling_algorithm, generation_count, execution_config, input_mode, input_path)
     print('==== PREDICTION OUTPUT ====')

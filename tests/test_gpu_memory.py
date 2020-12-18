9b55090f2bb3aeaab6eaae30866a2317505d9bf9
twilightdema@gmail.com <twilightdema@gmail.com>
2020-12-18 14:42:33 +0700

    Add GPU memory unit test

---
+++ tests/test_gpu_memory.py
@@ -0,0 +1,12 @@
+import subprocess as sp
+import os
+
+def get_gpu_memory():
+  _output_to_list = lambda x: x.decode('ascii').split('\n')[:-1]
+
+  COMMAND = "nvidia-smi --query-gpu=memory.free --format=csv"
+  memory_free_info = _output_to_list(sp.check_output(COMMAND.split()))[1:]
+  memory_free_values = [int(x.split()[0]) for i, x in enumerate(memory_free_info)]
+  return memory_free_values
+
+print(get_gpu_memory())

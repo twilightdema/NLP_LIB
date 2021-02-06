f2cdf2b679140b5d7d817f734e1b000f5a63e4cb
twilightdema@gmail.com <twilightdema@gmail.com>
2021-02-06 21:32:43 +0700

    wip

---
+++ tests/research_2_bottleneck/0_test_lr_scheduler.py
@@ -0,0 +1,24 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+from transformer_optimizer import GPTLearningRateSchedule, BERTLearningRateSchedule
+
+gpt_learning_rate_schedule = GPTLearningRateSchedule(512)
+bert_learning_rate_schedule = BERTLearningRateSchedule(               
+               initial_learning_rate = 0.005,
+               num_train_steps = 40000,
+               warmup_steps = 10000,
+               power=1
+               )
+
+plt.figure("GPT Learning Rate")
+plt.plot(gpt_learning_rate_schedule(tf.range(40000, dtype=tf.float32)))
+plt.ylabel("Learning Rate")
+plt.xlabel("Train Step")
+plt.show()
+
+plt.figure("BERT Learning Rate")
+plt.plot(bert_learning_rate_schedule(tf.range(40000, dtype=tf.float32)))
+plt.ylabel("Learning Rate")
+plt.xlabel("Train Step")
+plt.show()

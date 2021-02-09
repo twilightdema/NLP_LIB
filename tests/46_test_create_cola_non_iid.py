eb522887d0f9cef351667988a72ecb8577ffae97
twilightdema@gmail.com <twilightdema@gmail.com>
2021-02-09 14:48:31 +0700

    Add experiment 46

--- tests/46_test_create_cola_non_iid.py
+++
@@ -1,5 +0,0 @@
-'''
-This experiment is to try constructing CoLA dataset in non-iid federated training environment.
-The non-iid situation is that the input distribution is very diffent between node.
-This can be indicatd by different empirical token distribution input, or we can use KL-divergence too. 
-'''

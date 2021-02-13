9babcb54a53d1aaf7fd47b870b08676821f9e45a
twilightdema@gmail.com <twilightdema@gmail.com>
2021-02-13 13:31:35 +0700

    WIP

--- tests/research_2_bottleneck/1_implement_bottleneck_model.py
+++ tests/research_2_bottleneck/1_implement_bottleneck_model.py
@@ -1,7 +1,8 @@
 import numpy as np
 import tensorflow as tf
 import time
-from transformer import TransformerEncoder, TransformerBottleNeckEncoder, positional_encoding
+import random
+from transformer import TransformerEncoder, TransformerBottleNeckEncoder, positional_encoding, create_padding_mask
 from transformer_optimizer import create_bert_optimizer
 
 # Hyper-parameters
@@ -40,16 +41,11 @@ class TransformerBottleNeckClassifier(tf.keras.Model):
     self.fnn = tf.keras.layers.Dense(num_class_out)
 
   def call(self, x, training, mask):
+    mask = tf.cast(mask, tf.float32)
     out = self.transformer(x, training, mask)
     out = out[:, 0, :] # Pooled output (Gather only position of CLS token)
     out = self.fnn(out)
     return out
-
-# Try printing output size from example input
-encoder = TransformerBottleNeckClassifier(D_MODELS, N_HEADS, 2, 128)
-sample_output = encoder(
-    tf.random.uniform((64, 43, 16)), False, None)
-print(sample_output.shape)  # (batch_size, class_num)
 
 # Loss function for Multi-class Classifier
 loss_object = tf.keras.losses.CategoricalCrossentropy(
@@ -60,9 +56,20 @@ def loss_function(real, pred):
     return tf.reduce_mean(loss_)
 
 def accuracy_function(real, pred):
+    #print('real = ' + str(real))
+    #print('pred = ' + str(pred))
     accuracies = tf.equal(tf.argmax(real, axis=1), tf.argmax(pred, axis=1))
+    #print('accuracies = ' + str(accuracies))
     accuracies = tf.cast(accuracies, dtype=tf.float32)
+    #print('accuracies = ' + str(accuracies))
+    #print('accuracies = ' + str(tf.reduce_mean(accuracies)))
+    #exit(0)
     return tf.reduce_mean(accuracies)
+
+def create_input_mask(input):
+    # Encoder padding mask
+    enc_padding_mask = create_padding_mask(input)
+    return enc_padding_mask
 
 # Create metric object to hold metrics history
 train_loss = tf.keras.metrics.Mean(name='train_loss')
@@ -82,16 +89,49 @@ optimizer = create_bert_optimizer(
 # Create the model
 model = TransformerBottleNeckClassifier(D_MODELS, N_HEADS, OUTPUT_CLASS, SEQ_LEN)
 
-# Create sample input, mask and label
-test_input = tf.random.uniform((BATCH_SIZE, SEQ_LEN, D_MODELS[0]))
-test_mask = np.ones((BATCH_SIZE, SEQ_LEN), dtype=float)
-test_label = tf.random.uniform((BATCH_SIZE, OUTPUT_CLASS))
+# Simulation function
+def calculate_y(x):
+    sum = 0.0
+    for i in range(len(x)):
+        sum = sum + x[i][0]
+    sum = sum / len(x)
+    if sum > 0.5:
+        return [0, 1]
+    else:
+        return [1, 0]
+
+def simulate_training_data(seq_len, d_model, num):
+    xs = []
+    ys = []
+    for i in range(num):
+        x = []
+        for j in range(seq_len):
+            xx = random.random()
+            xx = [xx] * d_model
+            x.append(xx)
+        y = calculate_y(x)
+        xs.append(x)
+        ys.append(y)
+    return np.array(xs, dtype=float), np.array(ys, dtype=float)
+
+# Simulate training data
+test_input, test_label = simulate_training_data(SEQ_LEN, D_MODELS[0], 160)
+print('Training Input: ' + str(test_input.shape))
+print('Training Label: ' + str(test_label.shape))
+
+# Create input mask (directly from embedded input space)
+test_mask = np.zeros((160, 1, 1, SEQ_LEN), dtype=float)
+print('Training Mask: ' + str(test_mask.shape))
+
+# Create dataset from the numpy arrays
+train_dataset = tf.data.Dataset.from_tensor_slices((test_input, test_mask, test_label))
+train_batches = train_dataset.cache().batch(BATCH_SIZE).prefetch(tf.data.AUTOTUNE)
 
 def train_step(input, mask, label):
     with tf.GradientTape() as tape:
         predictions = model(input, 
                         True, 
-                        None)
+                        mask)
         loss = loss_function(label, predictions)
     gradients = tape.gradient(loss, model.trainable_variables)    
     optimizer.apply_gradients(zip(gradients, model.trainable_variables))
@@ -105,15 +145,11 @@ for epoch in range(EPOCHS):
     train_loss.reset_states()
     train_accuracy.reset_states()
 
-    '''
-    # inp -> portuguese, tar -> english
-    for (batch, (inp, tar)) in enumerate(train_batches):
-        train_step(inp, tar)
-    '''
-    train_step(test_input, test_mask, test_label)
+    for (batch, (x, mask, y)) in enumerate(train_batches):
+        train_step(x, mask, y)
 
-    #if batch % 50 == 0:
-    print(f'Epoch {epoch + 1} Loss {train_loss.result():.4f} Accuracy {train_accuracy.result():.4f}')
+    if batch % 5 == 0:
+        print(f'Epoch {epoch + 1} Batch {batch + 1} Loss {train_loss.result():.4f} Accuracy {train_accuracy.result():.4f}')
 
     '''
     if (epoch + 1) % 5 == 0:
@@ -122,6 +158,8 @@ for epoch in range(EPOCHS):
 
         print(f'Epoch {epoch + 1} Loss {train_loss.result():.4f} Accuracy {train_accuracy.result():.4f}')
     '''
+    print(f'Epoch {epoch + 1} Loss {train_loss.result():.4f} Accuracy {train_accuracy.result():.4f}')
+
     print(f'Time taken for 1 epoch: {time.time() - start:.2f} secs\n')
 
 print('Finished.')

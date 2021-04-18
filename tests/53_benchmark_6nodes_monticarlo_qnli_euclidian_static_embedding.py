a60d701d63d08e1dffe6c0b532774354bbb3545f
twilightdema@gmail.com <twilightdema@gmail.com>
2021-04-18 19:26:47 +0700

    Add more dataset for testing

--- tests/53_benchmark_6nodes_monticarlo_qnli_euclidian_static_embedding.py
+++ tests/53_benchmark_6nodes_monticarlo_qnli_euclidian_static_embedding.py
@@ -33,10 +33,10 @@ from textblob import Word
 tf.compat.v1.disable_eager_execution()
 
 # Experiment ID
-EXPERIMENT_ID = '51b'
+EXPERIMENT_ID = '53'
 
 # Task to be run
-TASK_NAME = 'cola'
+TASK_NAME = 'qnli'
 
 # Benchmark parameters
 TRIAL_NUM = 5
@@ -87,11 +87,11 @@ MATCH_USING_COSINE_SIMILARITY = True
 
 # Training Parameters
 COMMUNICATION_ROUNDS = 20
-LOCAL_TRAIN_EPOCH = 250 # 100
-ATTENTION_HEAD = 4
+LOCAL_TRAIN_EPOCH = 100
+ATTENTION_HEAD = 10 # 4
 BATCH_SIZE = 32
 BATCH_NUM = -1
-D_MODEL = 128
+D_MODEL = 500 # 400 # 128
 SEQ_LEN = -1 # -1 For automatically detected from training data maximum length
 VOCAB_SIZE = 150
 
@@ -530,6 +530,352 @@ with tf.device(USED_DEVICE):
       print('[INFO] No SPM model file, creating...')
       # Create raw corpus file to train SPM
       raw_corpus_file = os.path.join('dataset', 'mrpc', 'corpus.txt')
+      with open(raw_corpus_file, 'w', encoding='utf-8') as fout:
+        for data_row in data_train:
+          fout.write(data_row['input_1'] + '\n')
+          fout.write(data_row['input_2'] + '\n')
+        
+      # Train sentence piece model
+      spm.SentencePieceTrainer.Train('--pad_id=0 --bos_id=2 --eos_id=3 --unk_id=1 --user_defined_symbols=<MASK> --input=' + 
+        raw_corpus_file + 
+        ' --model_prefix=sp --vocab_size=' + str(max_dict_size) + ' --hard_vocab_limit=false')
+
+      # Delete raw corpus file
+      os.remove(raw_corpus_file)
+
+      # Move sp.model / sp.vocab to the dict paths
+      os.rename("sp.model", dict_model_path)
+      os.rename("sp.vocab", dict_vocab_path)
+
+      sentence_piece_processor.Load(dict_model_path)            
+    else:
+      sentence_piece_processor.Load(dict_model_path)
+
+    print('[INFO] Dictionary size = ' +str(sentence_piece_processor.GetPieceSize()))
+
+    # Perform encoding
+    for data in data_train:
+      encoded_data_1 = sentence_piece_processor.EncodeAsIds(data['input_1'])
+      encoded_data_2 = sentence_piece_processor.EncodeAsIds(data['input_2'])
+      encoded_data = encoded_data_1 + [TOKEN_SEP] + encoded_data_2
+      data['input_ids'] = encoded_data
+    for data in data_dev:
+      encoded_data_1 = sentence_piece_processor.EncodeAsIds(data['input_1'])
+      encoded_data_2 = sentence_piece_processor.EncodeAsIds(data['input_2'])
+      encoded_data = encoded_data_1 + [TOKEN_SEP] + encoded_data_2
+      data['input_ids'] = encoded_data
+
+    # Save pre-generated file
+    with open(encoded_data_train_path, 'wb') as fout:
+      pickle.dump(data_train, fout)
+    with open(encoded_data_dev_path, 'wb') as fout:
+      pickle.dump(data_dev, fout)
+
+    if PERFORM_DATA_BALANCING:
+      print('[INFO] Perform Data Balancing')
+      data_train = balance_training_data(data_train)
+      data_dev = balance_training_data(data_dev)
+
+    return data_train, data_dev
+
+  #################################################################
+  # FUNCTIONS FOR LOADING SST-2 DATASET
+  def read_sst2_data_file(file_path):
+    data = []
+    count = 0
+    with open(file_path, 'r', encoding='utf-8') as fin:
+      for line in fin:
+        if count == 0:
+          continue
+        columns = line.split('\t')
+        data_row = {
+          'id': str(count),
+          'label': columns[1].strip(),
+          'input': columns[0].strip()
+        }
+        print(data_row['input'] + ' => ' + data_row['label'])
+        data.append(data_row)
+        count = count + 1
+    return data
+
+  def load_sst2_data():
+    data_path_train = os.path.join('dataset_sst-2', 'train.tsv')
+    data_path_dev = os.path.join('dataset_sst-2', 'dev.tsv')
+    data_train = read_sst2_data_file(data_path_train)
+    data_dev = read_sst2_data_file(data_path_dev)
+    return data_train, data_dev
+
+  def load_encoded_sst2_data_static_word_embedding():
+    encoded_data_train_path = os.path.join('dataset', 'sst2', 'train_static_word_embedding.pk')
+    encoded_data_dev_path = os.path.join('dataset', 'sst2', 'dev_static_word_embedding.pk')
+
+    data_train = None
+    data_dev = None
+
+    if os.path.exists(encoded_data_train_path) and os.path.exists(encoded_data_dev_path):
+      # Load from processed file
+      print('[INFO] Loading data from pre-generated file.')
+      with open(encoded_data_train_path,'rb') as fin:
+        data_train = pickle.load(fin)
+      with open(encoded_data_dev_path,'rb') as fin:
+        data_dev = pickle.load(fin)
+
+      if PERFORM_DATA_BALANCING:
+        print('[INFO] Perform Data Balancing')
+        data_train = balance_training_data(data_train)
+        data_dev = balance_training_data(data_dev)
+
+      return data_train, data_dev
+
+    data_train_, data_dev_ = load_sst2_data()
+
+    # Perform word embedding, we remove all data entry those contain OOV words.
+    embedding_model = load_word_embedding_model()
+    data_train = []
+    data_dev = []
+    for data in data_train_:
+      encoded_data = perform_word_embedding(data['input'], embedding_model)
+      if encoded_data is not None:
+        data['input_ids'] = encoded_data
+        data_train.append(data)
+    for data in data_dev_:
+      encoded_data = perform_word_embedding(data['input'], embedding_model)
+      if encoded_data is not None:
+        data['input_ids'] = encoded_data
+        data_dev.append(data)
+
+    print('[INFO] ' + str(len(data_train)) + ' out of ' + str(len(data_train_)) + ' are embeddable (Train)...')
+    print('[INFO] ' + str(len(data_dev)) + ' out of ' + str(len(data_dev_)) + ' are embeddable (Dev)...')
+
+    # Save pre-generated file
+    with open(encoded_data_train_path, 'wb') as fout:
+      pickle.dump(data_train, fout)
+    with open(encoded_data_dev_path, 'wb') as fout:
+      pickle.dump(data_dev, fout)
+
+    if PERFORM_DATA_BALANCING:
+      print('[INFO] Perform Data Balancing')
+      data_train = balance_training_data(data_train)
+      data_dev = balance_training_data(data_dev)
+
+    return data_train, data_dev
+
+  def load_encoded_sst2_data_spm():
+    encoded_data_train_path = os.path.join('dataset', 'sst2', 'train.pk')
+    encoded_data_dev_path = os.path.join('dataset', 'sst2', 'dev.pk')
+
+    data_train = None
+    data_dev = None
+
+    if os.path.exists(encoded_data_train_path) and os.path.exists(encoded_data_dev_path):
+      # Load from processed file
+      print('[INFO] Loading data from pre-generated file.')
+      with open(encoded_data_train_path,'rb') as fin:
+        data_train = pickle.load(fin)
+      with open(encoded_data_dev_path,'rb') as fin:
+        data_dev = pickle.load(fin)
+
+      if PERFORM_DATA_BALANCING:
+        print('[INFO] Perform Data Balancing')
+        data_train = balance_training_data(data_train)
+        data_dev = balance_training_data(data_dev)
+
+      return data_train, data_dev
+
+    data_train, data_dev = load_sst2_data()
+    max_dict_size = VOCAB_SIZE
+    sentence_piece_processor = spm.SentencePieceProcessor()
+    print('[INFO] Max Dictionary Size = ' + str(max_dict_size))
+    dict_vocab_path = os.path.join('dataset', 'sst2', 'spm.vocab')
+    dict_model_path = os.path.join('dataset', 'sst2', 'spm.model')
+
+    if not os.path.exists(dict_model_path):
+      print('[INFO] No SPM model file, creating...')
+      # Create raw corpus file to train SPM
+      raw_corpus_file = os.path.join('dataset', 'sst2', 'corpus.txt')
+      with open(raw_corpus_file, 'w', encoding='utf-8') as fout:
+        for data_row in data_train:
+          fout.write(data_row['input'] + '\n')
+        
+      # Train sentence piece model
+      spm.SentencePieceTrainer.Train('--pad_id=0 --bos_id=' + str(TOKEN_CLS) + 
+        ' --eos_id=' + str(TOKEN_SEP) + 
+        ' --unk_id=' + str(TOKEN_UNKNOWN) + 
+        ' --user_defined_symbols=<MASK> --input=' + 
+        raw_corpus_file + 
+        ' --model_prefix=sp --vocab_size=' + str(max_dict_size) + ' --hard_vocab_limit=false')
+
+      # Delete raw corpus file
+      os.remove(raw_corpus_file)
+
+      # Move sp.model / sp.vocab to the dict paths
+      os.rename("sp.model", dict_model_path)
+      os.rename("sp.vocab", dict_vocab_path)
+
+      sentence_piece_processor.Load(dict_model_path)            
+    else:
+      sentence_piece_processor.Load(dict_model_path)
+
+    print('[INFO] Dictionary size = ' +str(sentence_piece_processor.GetPieceSize()))
+
+    # Perform encoding
+    for data in data_train:
+      encoded_data = sentence_piece_processor.EncodeAsIds(data['input'])
+      data['input_ids'] = encoded_data
+    for data in data_dev:
+      encoded_data = sentence_piece_processor.EncodeAsIds(data['input'])
+      data['input_ids'] = encoded_data
+
+    # Save pre-generated file
+    with open(encoded_data_train_path, 'wb') as fout:
+      pickle.dump(data_train, fout)
+    with open(encoded_data_dev_path, 'wb') as fout:
+      pickle.dump(data_dev, fout)
+
+    if PERFORM_DATA_BALANCING:
+      print('[INFO] Perform Data Balancing')
+      data_train = balance_training_data(data_train)
+      data_dev = balance_training_data(data_dev)
+
+    return data_train, data_dev
+
+  #################################################################
+  # FUNCTIONS FOR LOADING QNLI DATASET
+  def read_qnli_data_file(file_path):
+    data = []
+    i = 0
+    with open(file_path, 'r', encoding='utf-8') as fin:
+      for line in fin:
+        i = i + 1
+        if i == 1: continue # Skip header
+        columns = line.split('\t')
+        data_row = {
+          'id_1': columns[0].strip(),
+          'id_2': columns[0].strip(),
+          'label': ('1' if columns[3].strip() == 'entailment' else '0'),
+          'input_1': columns[1].strip(),
+          'input_2': columns[2].strip()
+        }
+        print(data_row['input_1'] + ', ' + data_row['input_2'] + ' => ' + data_row['label'])
+        data.append(data_row)
+    return data
+
+  def load_qnli_data():
+    data_path_train = os.path.join('dataset_qnli', 'train.txt')
+    data_path_dev = os.path.join('dataset_qnli', 'dev.txt')
+    data_train = read_qnli_data_file(data_path_train)
+    data_dev = read_qnli_data_file(data_path_dev)
+    return data_train, data_dev
+
+  def load_encoded_qnli_data_static_word_embedding():
+    encoded_data_train_path = os.path.join('dataset', 'qnli', 'train_static_word_embedding.pk')
+    encoded_data_dev_path = os.path.join('dataset', 'qnli', 'dev_static_word_embedding.pk')
+
+    data_train = None
+    data_dev = None
+
+    if os.path.exists(encoded_data_train_path) and os.path.exists(encoded_data_dev_path):
+      # Load from processed file
+      print('[INFO] Loading data from pre-generated file.')
+      with open(encoded_data_train_path,'rb') as fin:
+        data_train = pickle.load(fin)
+      with open(encoded_data_dev_path,'rb') as fin:
+        data_dev = pickle.load(fin)
+
+      if PERFORM_DATA_BALANCING:
+        print('[INFO] Perform Data Balancing')
+        data_train = balance_training_data(data_train)
+        data_dev = balance_training_data(data_dev)
+
+      return data_train, data_dev
+
+    data_train_, data_dev_ = load_qnli_data()
+
+    # Perform word embedding, we remove all data entry those contain OOV words.
+    embedding_model = load_word_embedding_model()
+    data_train = []
+    data_dev = []
+    for data in data_train_:
+      encoded_data_1 = perform_word_embedding(data['input_1'], embedding_model)
+      encoded_data_2 = perform_word_embedding(data['input_2'], embedding_model)
+      if encoded_data_1 is not None and encoded_data_2 is not None:
+        encoded_data = []
+        embedding_dimension = encoded_data_1.shape[1]
+        for w in encoded_data_1:
+          encoded_data.append(w)
+        encoded_data.append(np.array([TOKEN_SEP_STATIC_EMBEDDING] * embedding_dimension)) # TOKEN_SEP
+        for w in encoded_data_2:
+          encoded_data.append(w)
+        data['input_ids'] = np.array(encoded_data)
+        data_train.append(data)
+    for data in data_dev_:
+      encoded_data_1 = perform_word_embedding(data['input_1'], embedding_model)
+      encoded_data_2 = perform_word_embedding(data['input_2'], embedding_model)
+      if encoded_data_1 is not None and encoded_data_2 is not None:
+        encoded_data = []
+        embedding_dimension = encoded_data_1.shape[1]
+        for w in encoded_data_1:
+          encoded_data.append(w)
+        encoded_data.append(np.array([TOKEN_SEP_STATIC_EMBEDDING] * embedding_dimension)) # TOKEN_SEP
+        for w in encoded_data_2:
+          encoded_data.append(w)
+        data['input_ids'] = np.array(encoded_data)
+        data_dev.append(data)
+
+    print('[INFO] ' + str(len(data_train)) + ' out of ' + str(len(data_train_)) + ' are embeddable (Train)...')
+    print('[INFO] ' + str(len(data_dev)) + ' out of ' + str(len(data_dev_)) + ' are embeddable (Dev)...')
+
+    # Save pre-generated file
+    with open(encoded_data_train_path, 'wb') as fout:
+      pickle.dump(data_train, fout)
+    with open(encoded_data_dev_path, 'wb') as fout:
+      pickle.dump(data_dev, fout)
+
+    if PERFORM_DATA_BALANCING:
+      print('[INFO] Perform Data Balancing')
+      data_train = balance_training_data(data_train)
+      data_dev = balance_training_data(data_dev)
+
+    return data_train, data_dev
+
+  def load_encoded_qnli_data_spm():
+    encoded_data_train_path = os.path.join('dataset', 'qnli', 'train.pk')
+    encoded_data_dev_path = os.path.join('dataset', 'qnli', 'dev.pk')
+
+    data_train = None
+    data_dev = None
+
+    if os.path.exists(encoded_data_train_path) and os.path.exists(encoded_data_dev_path):
+      # Load from processed file
+      print('[INFO] Loading data from pre-generated file.')
+      with open(encoded_data_train_path,'rb') as fin:
+        data_train = pickle.load(fin)
+      with open(encoded_data_dev_path,'rb') as fin:
+        data_dev = pickle.load(fin)
+
+      if PERFORM_DATA_BALANCING:
+        print('[INFO] Perform Data Balancing')
+        data_train = balance_training_data(data_train)
+        data_dev = balance_training_data(data_dev)
+
+      return data_train, data_dev
+
+    data_folder = os.path.join('dataset', 'qnli')
+    if not os.path.exists(data_folder):
+      print('[INFO] No data folder found, recreating...')
+      os.makedirs(data_folder)
+
+    data_train, data_dev = load_qnli_data()
+    max_dict_size = 150
+    sentence_piece_processor = spm.SentencePieceProcessor()
+    print('[INFO] Max Dictionary Size = ' + str(max_dict_size))
+    dict_vocab_path = os.path.join('dataset', 'qnli', 'spm.vocab')
+    dict_model_path = os.path.join('dataset', 'qnli', 'spm.model')
+
+    if not os.path.exists(dict_model_path):
+      print('[INFO] No SPM model file, creating...')
+      # Create raw corpus file to train SPM
+      raw_corpus_file = os.path.join('dataset', 'qnli', 'corpus.txt')
       with open(raw_corpus_file, 'w', encoding='utf-8') as fout:
         for data_row in data_train:
           fout.write(data_row['input_1'] + '\n')
@@ -1712,6 +2058,10 @@ with tf.device(USED_DEVICE):
       return load_encoded_cola_data_spm, load_encoded_cola_data_static_word_embedding
     elif task_name == 'mrpc':
       return load_encoded_mrpc_data_spm, load_encoded_mrpc_data_static_word_embedding
+    elif task_name == 'sst2':
+      return load_encoded_sst2_data_spm, load_encoded_sst2_data_static_word_embedding
+    elif task_name == 'qnli':
+      return load_encoded_qnli_data_spm, load_encoded_qnli_data_static_word_embedding
     else:
       return None, None
 
